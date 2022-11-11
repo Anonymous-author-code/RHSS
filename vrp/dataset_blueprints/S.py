d74d1084c2761540d67ded68ca542714b45ec527
Anonymous-author-code <73868935+Anonymous-author-code@users.noreply.github.com>
2022-11-11 21:28:27 +0800

    Update S.py

--- vrp/dataset_blueprints/S.py
+++ vrp/dataset_blueprints/S.py
@@ -22,7 +22,7 @@ dataset['101A1'] = InstanceBlueprint(nb_customers=100, depot_position='C', custo
     demand_type='inter', demand_min=2, demand_max=47, capacity=160, grid_size=1)
 dataset['101A2'] = InstanceBlueprint(nb_customers=100, depot_position='C', customer_position='R', nb_customer_cluster=None,
     demand_type='inter', demand_min=16, demand_max=143, capacity=160, grid_size=1)
-dataset['101A3'] = InstanceBlueprint(nb_customers=100, depot_position='C', customer_position='R', nb_customer_cluster=None,
+dataset['101A3'] = InstanceBlueprint(nb_customers=100, depot_position='C', customer_position='R', nb_customer_cluster=None, 
     demand_type='inter', demand_min=2, demand_max=143, capacity=160, grid_size=1)
 
 
